b3c6967b2983231ad90849594597e768e5f9535a
Aly Soliman <alysoliman05@gmail.com>
2023-04-24 19:52:21 +0300

    adding background and navbar components to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import "./globals.scss";
 import Background from "./components/Background/background";
+import Navbar from "./components/Navbar/Navbar";
 
 export const metadata = {
   title: "ConfPlus",
@@ -8,9 +9,22 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const links = [
+    {
+      name: "Home",
+      link: "/",
+    },
+
+    {
+      name: "About",
+
+      link: "/",
+    },
+  ];
   return (
     <html lang="en">
       <body>
+        <Navbar links={links} />
         {children}
         <Background />
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -5,45 +5,5 @@ import styles from "./page.module.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <main>
-        <aside id="sectionIndicator">
-          <p>WELCOME</p>
-          <p>SCHEDULE</p>
-          <p>ABOUT</p>
-        </aside>
-        <section id="card"></section>
-      </main>
-
-      <div id="sign-in">
-        <aside></aside>
-        <section>
-          <p>Hi There, Welcome to ConfPlus</p>
-          <form action="submit">
-            <label htmlFor="email"></label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your Email"
-            />
-            <label htmlFor="password"></label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Enter your Password"
-            />
-            <button type="submit">Sign In</button>
-          </form>
-        </section>
-      </div>
-
-      <footer id="scroll">
-        <p></p>
-        <image src="../assets/test.png" class="test" alt="scroll"></image>
-      </footer>
-    </>
-  );
+  return <div></div>;
 }
