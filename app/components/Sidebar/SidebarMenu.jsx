3cc2d16e23101e70cbacf016678fd8e47fac17a2
hunzed <101815330+hunzed@users.noreply.github.com>
2023-04-25 13:49:38 +0300

    updated

---
+++ app/components/Sidebar/SidebarMenu.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from './sidebar.module.scss';
+
+export default function SidebarMenu() {
+  const [activeTab, setActiveTab] = useState('');
+
+  return (
+    <div className={styles.sidebarMenu}>
+      <button
+        className={`${styles.tabButton} ${
+          activeTab === 'welcome' ? styles.active : ''
+        }`}
+        onClick={() => setActiveTab('welcome')}
+      >
+        Welcome
+      </button>
+      <button
+        className={`${styles.tabButton} ${
+          activeTab === 'schedule' ? styles.active : ''
+        }`}
+        onClick={() => setActiveTab('schedule')}
+      >
+        Schedule
+      </button>
+      <button
+        className={`${styles.tabButton} ${
+          activeTab === 'about' ? styles.active : ''
+        }`}
+        onClick={() => setActiveTab('about')}
+      >
+        About
+      </button>
+    </div>
+  )
+}
