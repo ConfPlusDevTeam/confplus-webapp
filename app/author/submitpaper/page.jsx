fb23a21909bda28483264e5acec5d6c798fc625b
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-26 02:30:41 +0300

    feat: add organizer/author/reviewer pages

---
+++ app/author/submitpaper/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SubmitPaper() {
+  return <div>SubmitPaper</div>;
+}
---
+++ app/reviewer/reviewpaper/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReviewPaper() {
+  return <div>ReviewPaper</div>;
+}
