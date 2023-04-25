6ec0e112d20bda01cfb25ec6548da0192ee9773c
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-25 18:47:42 +0300

    feat(HomePage): content container component

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,22 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
+import ContentContainer from "./components/ContentContainer/ContentContainer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Sidebar />;
+  return (
+    <div className={styles.homeContainer}>
+      <Sidebar />
+      <div className={styles.card}>
+        <ContentContainer variant={1} title="Test">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
+          similique nisi maxime suscipit et numquam repudiandae aut, architecto
+          veniam eveniet blanditiis ex, voluptas fugiat laboriosam assumenda
+          illo illum debitis fuga?
+        </ContentContainer>
+      </div>
+    </div>
+  );
 }
--- app/testPage/page.jsx
+++ app/testPage/page.jsx
@@ -4,5 +4,12 @@ import Sidebar from "../components/Sidebar/Sidebar";
 import ContentContainer from "../components/ContentContainer/ContentContainer";
 
 export default function page() {
-  return <ContentContainer title="Test">Hi There!</ContentContainer>;
+  return (
+    <ContentContainer variant={1} title="Test">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed similique
+      nisi maxime suscipit et numquam repudiandae aut, architecto veniam eveniet
+      blanditiis ex, voluptas fugiat laboriosam assumenda illo illum debitis
+      fuga?
+    </ContentContainer>
+  );
 }
