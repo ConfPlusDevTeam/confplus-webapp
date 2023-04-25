dd0678604bfaa0d7f454e4afeaa8fee069df8a8a
Aly Soliman <alysoliman05@gmail.com>
2023-04-25 22:28:02 +0300

    adding tab container and NavLinks with styling

---
+++ app/components/Tabs/Tabs.jsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import styles from "./Tabs.module.scss";
+import NavLink from "../NavLink/NavLink";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { useState } from "react";
+
+export default function Tabs({ links }) {
+  const [selected, setSelected] = useState();
+  const segment = useSelectedLayoutSegment();
+  return (
+    <div className={styles.tabsContainer}>
+      <ul className={styles.links}>
+        {links.map((link, key) => (
+          <NavLink
+            navlink={link}
+            active={
+              selected == key || `${segment}` == link.link.replace("/", "")
+                ? true
+                : false
+            }
+            onclick={() => setSelected(key)}
+            key={key}
+            className={styles.link}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
--- app/testPage/page.jsx
+++ app/testPage/page.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
 import ContentContainer from "../components/ContentContainer/ContentContainer";
-
+import Tabs from "../components/Tabs/Tabs";
 export default function page() {
-  return (
-    <ContentContainer variant={1} title="Test">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed similique
-      nisi maxime suscipit et numquam repudiandae aut, architecto veniam eveniet
-      blanditiis ex, voluptas fugiat laboriosam assumenda illo illum debitis
-      fuga?
-    </ContentContainer>
-  );
+  const links = [
+    {
+      name: "Home",
+      link: "/",
+    },
+
+    {
+      name: "Test",
+
+      link: "/testPage",
+    },
+
+    {
+      name: "Sign In",
+
+      link: "/signin",
+    },
+  ];
+  return <Tabs links={links} />;
 }
