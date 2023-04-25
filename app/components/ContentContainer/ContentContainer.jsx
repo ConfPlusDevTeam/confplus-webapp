603b4a4f5ace5fc936dd85266ebe80a870bffbdc
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-25 16:02:33 +0300

    ContentContainer init

---
+++ app/components/ContentContainer/ContentContainer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./ContentContainer.module.scss";
+
+export default function ContentContainer({ children, title }) {
+  return <div>{children}</div>;
+}
--- app/testPage/page.jsx
+++ app/testPage/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
+import ContentContainer from "../components/ContentContainer/ContentContainer";
 
 export default function page() {
-  return;
+  return <ContentContainer title="Test">Hi There!</ContentContainer>;
 }
