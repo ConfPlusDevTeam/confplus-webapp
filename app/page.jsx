59317f123ff444b503b6fdb87a89210e7fdbce5f
hunzed <hunzalahhassan@gmail.com>
2023-04-25 23:07:32 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,6 @@ import styles from "./page.module.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
 import ContentContainer from "./components/ContentContainer/ContentContainer";
 import { useState } from "react";
-import Scrollbar from "./components/Scrollbar/Scrollbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
