0536d4b529d325f508d482aa63e32dd24117c6c9
Aly Soliman <alysoliman05@gmail.com>
2023-04-25 22:19:27 +0300

    add styles as props to NavLink component

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -24,6 +24,7 @@ export default function Navbar({ links }) {
             }
             onclick={() => setSelected(key)}
             key={key}
+            className={styles.link}
           />
         ))}
       </ul>
