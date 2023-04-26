8c2d029f555e432545f20fa4f89f2a9669ae7b62
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-26 17:37:20 +0300

    styles(overflow): make overflow y hidden only in HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -6,26 +6,37 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import ContentContainer from "./components/ContentContainer/ContentContainer";
 import { useState } from "react";
 import Scrollbar from "./components/Scrollbar/Scrollbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [active, setActive] = useState("welcome");
   return (
-    <div className={styles.homeContainer}>
-      <Sidebar
-        onclick={(activeTab) => setActive(activeTab)}
-        activeElement={active}
-      />
-      <div className={styles.card}>
-        <ContentContainer variant={1} title="Test">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
-          similique nisi maxime suscipit et numquam repudiandae aut, architecto
-          veniam eveniet blanditiis ex, voluptas fugiat laboriosam assumenda
-          illo illum debitis fuga?
-        </ContentContainer>
-        <Scrollbar />
+    <>
+      <head>
+        {" "}
+        <style>{`
+        body {
+          overflow:hidden
+        }
+`}</style>
+      </head>
+      <div className={styles.homeContainer}>
+        <Sidebar
+          onclick={(activeTab) => setActive(activeTab)}
+          activeElement={active}
+        />
+        <div className={styles.card}>
+          <ContentContainer variant={1} title="Test" className={styles}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
+            similique nisi maxime suscipit et numquam repudiandae aut,
+            architecto veniam eveniet blanditiis ex, voluptas fugiat laboriosam
+            assumenda illo illum debitis fuga?
+          </ContentContainer>
+          <Scrollbar />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
