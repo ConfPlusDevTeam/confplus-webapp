6460d100b9eb1f28f2ead533c8494552d83f04f4
hunzed <hunzalahhassan@gmail.com>
2023-04-26 18:04:06 +0300

    Update UsersRepo.js

--- app/api/UsersRepo.js
+++ app/api/UsersRepo.js
@@ -1,23 +1,23 @@
-//getUsers() -> returns an array of all users from users.json
-//getUsersByRole(Role) -> an array of all users that are of that role - publishers, reviewer...
-//validateUser (email, password) -> returns the user if the email and password match, otherwise returns null
-
 const fs = require('fs');
-const usersPath = '../data/users.json';
+const path = require('path');
+const usersPath = path.join(__dirname, '../data/users.json');
 
-export const getUsers = () => {
-    return JSON.parse(fs.readFile(usersPath));
+const getUsers = () => {
+    return JSON.parse(fs.readFileSync(usersPath));
 }
 
-export const getUsersByRole = (role) => {
+const getUsersByRole = (role) => {
     return getUsers().filter(user => user.role === role);
 }
 
-export const validateUser = (email, password) => {
+const validateUser = (email, password) => {
     return getUsers().find(user => user.email === email && user.password === password);
 }
 
+module.exports = {
+    getUsers,
+    getUsersByRole,
+    validateUser
+};
 
-
-
-
+console.log(getUsers());
