21130bb4f9d8c485db40447e0a9dcd6c6cfd406e
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-26 15:03:51 +0300

    feat(navbar): make navbar signin/account link switch automatically

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
+// "use client";
 import Link from "next/link";
 import "./globals.scss";
 import Background from "./components/Background/background";
 import Navbar from "./components/Navbar/Navbar";
+// import { useState } from "react";
 
 export const metadata = {
   title: "ConfPlus",
@@ -33,6 +35,14 @@ export default function RootLayout({ children }) {
       link: "/signin",
     },
   ];
+
+  // const [isSignedIn, setIsSignedIn] = useState();
+  // setIsSignedIn(from localStorage);
+  // if (isSignedIn) {
+  //   links[3].name = "Account";
+  //   links[3].link = "/author"; // get the role from the local storage
+  // }
+
   return (
     <html lang="en">
       <body>
