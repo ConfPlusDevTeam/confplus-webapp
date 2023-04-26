17d4217c5ea010b4716bc52caaa173dd4fc667a1
Aly Soliman <alysoliman05@gmail.com>
2023-04-27 02:15:02 +0300

    fix: fix styles

--- app/components/SubmitPaperForm/SubmitPaperForm.jsx
+++ app/components/SubmitPaperForm/SubmitPaperForm.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import styles from "./SubmitPaperForm.module.scss";
 export default function SubmitPaperForm() {
   return (
-    <form>
-      <h3> SUBMIT FORM</h3>
+    <form className={styles.form}>
+      <h3 className={styles.submitForm}> SUBMIT FORM</h3>
       <h4 className={styles.paperDetails}>PAPER DETAILS:</h4>
-      <label htmlFor="">TITLE</label>
+      <label className={styles.label}>TITLE:</label>
+      <input className={styles.input} type="text" placeholder="Enter Title" />
     </form>
   );
 }
