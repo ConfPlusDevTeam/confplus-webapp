7482968589879c005eb4b3eba7b3e1e09c0587c6
hunzed <hunzalahhassan@gmail.com>
2023-04-27 19:28:01 +0300

    Update route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -5,8 +5,15 @@ const repo = new UsersRepo();
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const role = searchParams.get("role");
-  if (role) {
-    return Response.json(await repo.getUsersByRole(role));
-  }
-  return Response.json(await repo.getUsers());
+    try{
+        if (role) {
+            if (role !== "reviewer" || role !== "author" || role !== "organizers") 
+                return Response.json(await repo.getUsersByRole(role), { status: 200 });
+            else 
+                return Response.json({ message: "Invalid role" }, { status: 400 });
+        }
+        return Response.json(await repo.getUsers());
+    } catch (e) {
+        return Response.json({ message: "Internal server error" }, { status: 500 });
+    }
 }
