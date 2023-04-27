877f46679ba59ead087a7b5341e6ba35b1478d9a
hunzed <hunzalahhassan@gmail.com>
2023-04-27 19:02:11 +0300

    Update route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -5,7 +5,9 @@ export async function GET (request , {params}) {
     try{
         const role = new URL (request.url).searchParams.get("role").toLowerCase;
         if (role) {
+            if (role === "author" || role === "reviewer" || role === "orgnaizer") 
             return Response.Json(repo.getUsersByRole(role),{status: 200})
+            else return ({message: "invalid role"}, {status: 400});
         }
         return (repo.getUsers(),{status: 200});
     }
