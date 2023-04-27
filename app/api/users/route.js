e260cf392303535ccc1a40bf031253476f56f0ec
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-27 19:12:42 +0300

    fix(api): fix /users

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,18 +1,12 @@
 //get users - returns all users if no query string parameters are passed else return users matching query of role type
+import UsersRepo from "./users-repo";
+const repo = new UsersRepo();
 
-import * as repo from "./UsersRepo.js";
-export async function GET (request , {params}) {
-    try{
-        const role = new URL (request.url).searchParams.get("role").toLowerCase;
-        if (role) {
-            if (role === "author" || role === "reviewer" || role === "orgnaizer") 
-            return Response.Json(repo.getUsersByRole(role),{status: 200})
-            else return ({message: "invalid role"}, {status: 400});
-        }
-        return (repo.getUsers(),{status: 200});
-    }
-    catch (e) {
-        return ({message: "internal server error"}, {status: 500});
-    }
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const role = searchParams.get("role");
+  if (role) {
+    return Response.json(await repo.getUsersByRole(role));
+  }
+  return Response.json(await repo.getUsers());
 }
-
