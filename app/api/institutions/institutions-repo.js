c72722772514697241a03a05361d62e227b54d5a
hunzed <hunzalahhassan@gmail.com>
2023-04-28 01:34:43 +0300

    institutions api done

---
+++ app/api/institutions/institutions-repo.js
@@ -0,0 +1,13 @@
+//get institutions
+import fs from "fs-extra";
+import { nanoid } from "nanoid";
+import path from "path";
+
+export default class InstituionsRepo {
+  constructor() {
+    this.path = path.join(process.cwd(), "app/data/institutions.json");
+  }
+  async getInstitutions() {
+    return JSON.parse(await fs.readFile(this.path));
+  }
+}
--- app/api/institutions/route.js
+++ app/api/institutions/route.js
@@ -1,1 +1,14 @@
-//get institutions
+//Get -returns all institutions
+
+import InstituionsRepo from "./institutions-repo";
+const institutionsRepo = new InstituionsRepo();
+
+export async function GET(request, { params }) {
+    try {
+        const institutions = await institutionsRepo.getInstitutions();
+        return Response.json(institutions, { status: 200 });
+    }
+    catch (e) {
+        return Response.json({ message: "Internal server error" }, { status: 500 });
+    }
+}
