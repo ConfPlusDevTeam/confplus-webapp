0f40357ae12ed6ed462f297bd6e8db482db5f467
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-29 02:18:00 +0300

    feat(submit paper): fix and implement the submission form

--- app/components/SubmitPaperForm/SubmitPaperForm.jsx
+++ app/components/SubmitPaperForm/SubmitPaperForm.jsx
@@ -4,102 +4,88 @@ import Button from "../Button/Button";
 import styles from "./SubmitPaperForm.module.scss";
 import UploadField from "../UploadField/UploadField";
 import { useState } from "react";
+import { useEffect } from "react";
 
-export default function SubmitPaperForm({ affiliations }) {
+export default function SubmitPaperForm({}) {
   const [coAuthors, setCoAuthors] = useState([]);
   const [showButton, setShowButton] = useState(true);
-  const elements = [];
-  const [count, setCount] = useState(0);
+  const [showForm, setShowForm] = useState(false);
+  const [affiliations, setAffiliations] = useState([]);
+  const [emails, setEmails] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const getAffiliations = async () => {
+    const response = await fetch("/api/institutions").then((response) =>
+      response.json()
+    );
+    setAffiliations(await response);
+  };
+
+  const getUsers = async () => {
+    const response = await fetch("/api/users?role=author").then((response) =>
+      response.json()
+    );
+    setUsers(await response);
+    setEmails((await response).map((email) => email.email));
+  };
+
+  useEffect(() => {
+    getAffiliations();
+    getUsers();
+  }, []);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [affiliation, setAffiliation] = useState("");
+  const [markPresenter, setMarkPresenter] = useState(false);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setName(
+      users.filter((user) => user.email == event.target.value)[0].first_name +
+        " " +
+        users.filter((user) => user.email == event.target.value)[0].last_name
+    );
+  };
+  const handleAffiliationChange = (event) => {
+    setAffiliation(event.target.value);
+  };
+  const handleMarkPresenterChange = (event) => {
+    setMarkPresenter(!markPresenter);
+  };
 
   const handleShowButton = () => {
     setShowButton(!showButton);
   };
 
-  const handleClick = () => {
-    setCount(count + 1);
+  const handleShow = () => {
+    setShowForm(!showForm);
   };
 
-  const handleSubmit = (event) => {
+  const handleAdd = (event) => {
     event.preventDefault();
-    const newCoAuthor = {
-      name: event.target[0].value,
-      email: event.target[1].value,
-      affiliation: event.target[2].value,
-      markPresenter: event.target[3].value,
-    };
-    console.log(newCoAuthor);
-    setCoAuthors([...coAuthors, newCoAuthor]);
+    if (name && email && affiliation) {
+      const newCoAuthor = {
+        name: name,
+        email: email,
+        affiliation: affiliation,
+        presenter: markPresenter ? "yes" : "no",
+      };
+      setCoAuthors([...coAuthors, newCoAuthor]);
+      setName("");
+      setEmail("");
+      setAffiliation("");
+      setMarkPresenter(false);
+    }
   };
 
   const handleDelete = (index) => {
     const newCoAuthors = [...coAuthors];
     newCoAuthors.splice(index, 1);
     setCoAuthors(newCoAuthors);
-    elements.splice(index, 1);
-  };
-
-  const renderHTML = () => {
-    for (let i = 0; i < count; i++) {
-      elements.push(
-        <div className={styles.coAuthorForm} onSubmit={handleSubmit}>
-          <h4 className={styles.paperDetails}>CO-AUTHOR {count}</h4>
-          <div>
-            <label htmlFor="name" className={styles.label}>
-              NAME:
-            </label>
-            <input
-              id="name"
-              className={styles.input}
-              type="text"
-              placeholder="Enter Name"
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className={styles.label}>
-              EMAIL:
-            </label>
-            <input
-              id="email"
-              className={styles.input}
-              type="email"
-              placeholder="Enter Email"
-            />
-          </div>
-          <div>
-            <label htmlFor="affiliation" className={styles.label}>
-              AFFILIATION:
-            </label>
-            <select
-              id="affiliation"
-              name="affiliation"
-              className={styles.affiliations}
-            >
-              {affiliations?.map((affiliation) => (
-                <option value={affiliation}>{affiliation}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="mark-presenter" className={styles.label}>
-              Mark as Presenter:
-            </label>
-            <input
-              type="checkbox"
-              id="mark-presenter"
-              className={styles.checkbox}
-            />
-          </div>
-          <Button
-            className={styles.saveBtn}
-            variant={1}
-            type="submit"
-            text="Save"
-            onClick={handleSubmit}
-          ></Button>
-        </div>
-      );
-    }
-    return elements;
   };
 
   return (
@@ -132,22 +118,43 @@ export default function SubmitPaperForm({ affiliations }) {
         <UploadField></UploadField>
       </div>
       <div>
-        <label htmlFor="title" className={styles.label}>
+        <label htmlFor="link" className={styles.label}>
           FILE LINK:
         </label>
         <input
-          id="title"
+          id="link"
           className={styles.input}
           type="text"
           placeholder="Enter Link"
         />
       </div>
+      {coAuthors.length > 0 && (
+        <div className={styles.showCoAuthors}>
+          <h5>CoAuthors:</h5>
+          <div className={styles.coAuthorContainer}>
+            {coAuthors?.map((coAuthor, index) => (
+              <div key={index}>
+                <p className={styles.coAuthor}>
+                  {index + 1 + "- "}
+                  {coAuthor.name} ({coAuthor.email}){" "}
+                </p>{" "}
+                <span
+                  onClick={() => handleDelete(index)}
+                  className={styles.deleteCoAuthor}
+                >
+                  X
+                </span>
+              </div>
+            ))}{" "}
+          </div>
+        </div>
+      )}
 
       <div>
         <div>
           <input
             type={"button"}
-            onClick={handleClick}
+            onClick={handleShow}
             value="+"
             id="add-coauthor"
             className={styles.addCoAuthorBtn}
@@ -155,7 +162,94 @@ export default function SubmitPaperForm({ affiliations }) {
           <label htmlFor="add-coauthor" className={styles.paperDetails}>
             ADD CO-AUTHOR
           </label>
-          {renderHTML().map((element) => element)}
+          {showForm && (
+            <div className={styles.coAuthorForm}>
+              <h4 className={styles.paperDetails}>CO-AUTHOR </h4>
+
+              <div>
+                {/* <label htmlFor="email" className={styles.label}>
+                  EMAIL:
+                </label>
+                <input
+                  id="email"
+                  className={styles.input}
+                  type="email"
+                  placeholder="Enter Email"
+                  onChange={handleEmailChange}
+                  value={email}
+                /> */}
+                <label htmlFor="email" className={styles.label}>
+                  EMAIL:
+                </label>
+                <select
+                  id="email"
+                  name="email"
+                  className={styles.affiliations}
+                  value={email}
+                  onChange={handleEmailChange}
+                >
+                  {emails?.map((em, key) => (
+                    <option key={key} onClick={handleEmailChange} value={em}>
+                      {em}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <label htmlFor="name" className={styles.label}>
+                  NAME:
+                </label>
+                <input
+                  id="name"
+                  className={styles.input}
+                  type="text"
+                  disabled
+                  value={name}
+                />
+              </div>
+              <div>
+                <label htmlFor="affiliation" className={styles.label}>
+                  AFFILIATION:
+                </label>
+                <select
+                  id="affiliation"
+                  name="affiliation"
+                  className={styles.affiliations}
+                  value={affiliation}
+                  onChange={handleAffiliationChange}
+                >
+                  {affiliations?.map((affil, key) => (
+                    <option
+                      key={key}
+                      onClick={handleAffiliationChange}
+                      value={affil}
+                    >
+                      {affil}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <label htmlFor="mark-presenter" className={styles.label}>
+                  Mark as Presenter:
+                </label>
+                <input
+                  type="checkbox"
+                  id="mark-presenter"
+                  className={styles.checkbox}
+                  onChange={handleMarkPresenterChange}
+                  checked={markPresenter}
+                />
+              </div>
+              <Button
+                className={styles.saveBtn}
+                variant={1}
+                type="submit"
+                text="Save"
+                onClick={handleAdd}
+              ></Button>
+            </div>
+          )}
         </div>
       </div>
     </form>
