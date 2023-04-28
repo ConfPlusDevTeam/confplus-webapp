5091d9e999dac6664b2ac201e0e5c208dda62db3
Aly Soliman <alysoliman05@gmail.com>
2023-04-28 18:54:48 +0300

    create Files

--- app/reviewer/page.jsx
+++ app/reviewer/page.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import ContentContainer from "@/app/components/ContentContainer/ContentContainer";
+import Tabs from "@/app/components/Tabs/Tabs";
+import WelcomeMessage from "@/app/components/WelcomeMessage/WelcomeMessage";
+import styles from "./page.module.scss";
 
 export default function Reviewer() {
-  return <div>Reviewer</div>;
+  const links = [
+    {
+      name: "Assigned Papers",
+      link: "/reviewer",
+    },
+    {
+      name: "Reviewed Paper",
+      link: "/reviewer/reviewpaper",
+    },
+  ];
+  return (
+    <div className={styles.profile}>
+      <WelcomeMessage props="Aly" />
+      <ContentContainer variant={2} className={styles}>
+        <Tabs links={links} className={styles} />
+      </ContentContainer>
+    </div>
+  );
 }
--- app/reviewer/reviewpaper/page.jsx
+++ app/reviewer/reviewpaper/page.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import Reviewer from "../page";
+import ReviewPaperForm from "../../components/ReviewPaperForm/ReviewPaperForm";
+
 export default function ReviewPaper() {
-  return <div>ReviewPaper</div>;
+  return (
+    <Reviewer>
+      <ReviewPaperForm></ReviewPaperForm>
+    </Reviewer>
+  );
 }
