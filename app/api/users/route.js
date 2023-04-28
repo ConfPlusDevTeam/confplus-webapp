3a81385ddef40387458e08a84e420e4d38c24fdd
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-04-28 04:40:35 +0300

    fix(api): fix imports in /validate and change request type

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,19 +1,19 @@
 //get users - returns all users if no query string parameters are passed else return users matching query of role type
 
-import * as repo from "./users-repo";
+import UsersRepo from "./users-repo";
+const repo = new UsersRepo();
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const role = searchParams.get("role");
-    try{
-        if (role) {
-            if (role !== "reviewer" || role !== "author" || role !== "organizers") 
-                return Response.json(await repo.getUsersByRole(role), { status: 200 });
-            else 
-                return Response.json({ message: "Invalid role" }, { status: 400 });
-        }
-        return Response.json(await repo.getUsers());
-    } catch (e) {
-        return Response.json({ message: "Internal server error" }, { status: 500 });
+  try {
+    if (role) {
+      if (role !== "reviewer" || role !== "author" || role !== "organizers")
+        return Response.json(await repo.getUsersByRole(role), { status: 200 });
+      else return Response.json({ message: "Invalid role" }, { status: 400 });
     }
+    return Response.json(await repo.getUsers());
+  } catch (e) {
+    return Response.json({ message: "Internal server error" }, { status: 500 });
+  }
 }
--- app/api/users/users-repo.js
+++ app/api/users/users-repo.js
@@ -1,23 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const usersPath = path.join(__dirname, '../../data/users.json');
+import fs from "fs-extra";
+import { nanoid } from "nanoid";
+import path from "path";
 
-const getUsers = () => {
-    return JSON.parse(fs.readFileSync(usersPath));
+export default class UsersRepo {
+  constructor() {
+    this.path = path.join(process.cwd(), "app/data/users.json");
+  }
+  async getUsers() {
+    return JSON.parse(await fs.readFile(this.path));
+  }
+
+  async getUsersByRole(role) {
+    return (await this.getUsers()).filter((user) => user.role === role);
+  }
+
+  async validateUser(email, password) {
+    return (await this.getUsers()).find(
+      (user) => user.email === email && user.password === password
+    );
+  }
 }
-
-const getUsersByRole = (role) => {
-    return getUsers().filter(user => user.role === role);
-}
-
-const validateUser = (email, password) => {
-    return getUsers().find(user => user.email === email && user.password === password);
-}
-
-module.exports = {
-  getUsers,
-  getUsersByRole,
-  validateUser
-}
-
-console.log(getUsersByRole('author'));
