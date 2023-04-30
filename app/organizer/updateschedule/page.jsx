18906287d5216f2b8486f1f8497f852f0e300fd1
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-05-01 01:18:38 +0300

    feat: update schedule

--- app/organizer/updateschedule/page.jsx
+++ app/organizer/updateschedule/page.jsx
@@ -23,274 +23,319 @@ export default function UpdateSchedule() {
     }
   }, []);
 
-  const [coAuthors, setCoAuthors] = useState([]);
-  const [showButton, setShowButton] = useState(true);
+  const [dates, setDates] = useState([]);
+  const [date, setDate] = useState("");
+
+  const [schedule, setSchedule] = useState([]);
+
+  const [sessions, setSessions] = useState([]);
+  const [session, setSession] = useState({});
+
+  const [papers, setPapers] = useState([]);
+  const [paper, setPaper] = useState({});
+
+  const handlePaperChange = (e) => {
+    setPaper(e.target.value);
+    getSessions(date);
+  };
+
+  const [session2, setSession2] = useState({});
+
+  const handleSessionChange = (e) => {
+    setSession2(e.target.value);
+  };
+
   const [showForm, setShowForm] = useState(false);
-  const [affiliations, setAffiliations] = useState([]);
-  const [emails, setEmails] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const getAffiliations = async () => {
-    const response = await fetch("/api/institutions").then((response) =>
-      response.json()
-    );
-    setAffiliations(await response);
-  };
-
-  const getUsers = async () => {
-    const response = await fetch("/api/users?role=author").then((response) =>
-      response.json()
-    );
-    setUsers(await response);
-    setEmails((await response).map((email) => email.email));
-  };
-
-  useEffect(() => {
-    getAffiliations();
-    getUsers();
-  }, []);
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [affiliation, setAffiliation] = useState("");
-  const [markPresenter, setMarkPresenter] = useState(false);
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-    setName(
-      users.filter((user) => user.email == event.target.value)[0].first_name +
-        " " +
-        users.filter((user) => user.email == event.target.value)[0].last_name
-    );
-  };
-  const handleAffiliationChange = (event) => {
-    setAffiliation(event.target.value);
-  };
-  const handleMarkPresenterChange = (event) => {
-    setMarkPresenter(!markPresenter);
-  };
-
-  const handleShowButton = () => {
-    setShowButton(!showButton);
-  };
-
   const handleShow = () => {
     setShowForm(!showForm);
   };
 
-  const handleAdd = (event) => {
-    event.preventDefault();
-    if (name && email && affiliation) {
-      const newCoAuthor = {
-        name: name,
-        email: email,
-        affiliation: affiliation,
-        presenter: markPresenter ? "yes" : "no",
-      };
-      emails.splice(emails.indexOf(email), 1);
-      setErrorMessage("");
-      setCoAuthors([...coAuthors, newCoAuthor]);
-      setName("");
-      setEmail("");
-      setAffiliation("");
-      setMarkPresenter(false);
-    } else setErrorMessage("Please fill all the fields");
+  const [showForm2, setShowForm2] = useState(false);
+  const handleShow2 = () => {
+    setShowForm2(!showForm2);
   };
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [errorMessage2, setErrorMessage2] = useState("");
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+    getSessions(e.target.value);
+  };
+
+  const [locations, setLocations] = useState([]);
+  const [location, setLocation] = useState("");
+
+  const [name, setName] = useState("");
+  const [sTime, setSTime] = useState("");
+  const [fTime, setFTime] = useState("");
+
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const getLocations = async () => {
+    const response = await fetch("/api/locations")
+      .then((response) => response.json())
+      .then((data) => {
+        setLocations(data);
+      });
+  };
+
+  const getDates = async () => {
+    const response = await fetch("/api/schedule/dates").then((response) =>
+      response.json()
+    );
+    setDates(await response);
+  };
+
+  const getSessions = async (date) => {
+    const response = await fetch(`/api/schedule`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSchedule(data);
+        return data.find((day) => day.name == date);
+      })
+      .then((data) => {
+        console.log(data);
+        return data.sessions;
+      });
+
+    setSessions(await response);
+  };
+
+  const getAcceptedPapers = async (session) => {
+    const response = await fetch(`/api/papers/accepted`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPapers(data);
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    getDates();
+    getLocations();
+    getAcceptedPapers();
+  }, []);
 
   const handleDelete = (index) => {
-    const newCoAuthors = [...coAuthors];
-    emails.push(newCoAuthors[index].email);
-
-    newCoAuthors.splice(index, 1);
-    setCoAuthors(newCoAuthors);
+    const list = [...sessions];
+    list.splice(index, 1);
+    setSessions(list);
   };
 
-  const handleSubmit = (event) => {
+  const handleAdd = (event) => {
     event.preventDefault();
-    const paper = {
-      paperTitle: event.target.title.value,
-      abstract: event.target.abstract.value,
-      fileLink: event.target.link.value,
-      author: JSON.parse(localStorage.getItem("user")).email,
-      coAuthors: coAuthors,
-    };
-    fetch("/api/papers", {
+
+    if (location === "") {
+      setErrorMessage("Please choose a location");
+      return;
+    }
+    if (sTime === "") {
+      setErrorMessage("Please choose a starting time");
+      return;
+    }
+    if (fTime === "") {
+      setErrorMessage("Please choose a finishing time");
+      return;
+    }
+    if (name === "") {
+      setErrorMessage("Please enter a session name");
+      return;
+    }
+    const list = [...sessions];
+    list.push({
+      name: name,
+      location: location,
+      fromTime: sTime,
+      endTime: fTime,
+      presenters: [
+        {
+          name: "Presenter 1",
+        },
+        {
+          name: "Presenter 2",
+        },
+      ],
+      assignedPapers: [
+        {
+          title: "Paper 1",
+        },
+        {
+          title: "Paper 2",
+        },
+      ],
+    });
+    setSessions(list);
+    setShowForm(false);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (date === "") {
+      alert("Please choose a date");
+      return;
+    }
+    if (sessions.length === 0) {
+      alert("Please add at least one session");
+      return;
+    }
+    const response = await fetch("/api/schedule", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(paper),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-        const user = JSON.parse(localStorage.getItem("user"));
-        router.push(`/${user.role}`);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("Error submitting paper");
-      });
+      body: JSON.stringify([
+        {
+          name: date,
+          sessions: sessions,
+        },
+      ]),
+    }).then((response) => response.json());
+    if (response.error) {
+      setErrorMessage(response.error);
+      return;
+    }
+    router.push("/schedule");
   };
 
   return (
     <ContentContainer variant={2} className={styles}>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <h3 className={styles.submitForm}> SUBMIT FORM</h3>
-        <h4 className={styles.paperDetails}>PAPER DETAILS:</h4>
+        <h3 className={styles.submitForm}> Update Schedule</h3>
         <div>
-          <label htmlFor="title" className={styles.label}>
-            TITLE:
-          </label>
-          <input
-            id="title"
-            className={styles.input}
-            type="text"
-            placeholder="Enter Title"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="abstract" className={styles.label}>
-            ABSTRACT:
-          </label>
-          <textarea
-            id="abstract"
-            className={styles.textArea}
-            type="text"
-            placeholder="Enter Abstract"
-            required
-          />
-        </div>
-
-        <div>
-          <label htmlFor="link" className={styles.label}>
-            FILE LINK:
-          </label>
-          <input
-            id="link"
-            className={styles.input}
-            type="url"
-            placeholder="Enter Link"
-            required
-          />
-        </div>
-        {coAuthors.length > 0 && (
-          <div className={styles.showCoAuthors}>
-            <h5>CoAuthors:</h5>
-            <div className={styles.coAuthorContainer}>
-              {coAuthors?.map((coAuthor, index) => (
-                <div key={index}>
-                  <p className={styles.coAuthor}>
-                    {index + 1 + "- "}
-                    {coAuthor.name} ({coAuthor.email}){" "}
-                  </p>{" "}
-                  <span
-                    onClick={() => handleDelete(index)}
-                    className={styles.deleteCoAuthor}
-                  >
-                    X
-                  </span>
-                </div>
-              ))}{" "}
-            </div>
+          <div>
+            <label htmlFor="day" className={styles.label}>
+              Date:
+            </label>
+            <select
+              id="day"
+              name="day"
+              className={styles.affiliations}
+              value={date}
+              onChange={handleDateChange}
+              defaultValue=""
+            >
+              <option disabled hidden value="">
+                Choose Date
+              </option>
+              {dates?.map((em, key) => (
+                <option key={key} onClick={handleDateChange} value={em}>
+                  {em}
+                </option>
+              ))}
+            </select>
           </div>
-        )}
+          {sessions?.length > 0 && (
+            <div className={styles.showCoAuthors}>
+              <h5>Sessions:</h5>
+              <div className={styles.coAuthorContainer}>
+                {sessions?.map((session, index) => (
+                  <div key={index}>
+                    <p className={styles.coAuthor}>
+                      {index + 1 + "- "}
+                      {session.name} (Starting time: {session.fromTime}){" "}
+                    </p>{" "}
+                    <span
+                      onClick={() => handleDelete(index)}
+                      className={styles.deleteCoAuthor}
+                    >
+                      X
+                    </span>
+                  </div>
+                ))}{" "}
+              </div>
+            </div>
+          )}
+        </div>
 
         <div>
           <div>
-            <input
-              type={"button"}
-              onClick={handleShow}
-              value="+"
-              id="add-coauthor"
-              className={styles.addCoAuthorBtn}
-            ></input>
-            <label htmlFor="add-coauthor" className={styles.paperDetails}>
-              ADD CO-AUTHOR
-            </label>
+            {sessions?.length > 0 && (
+              <>
+                <input
+                  type={"button"}
+                  onClick={handleShow}
+                  value="+"
+                  id="add-coauthor"
+                  className={styles.addCoAuthorBtn}
+                ></input>
+                <label htmlFor="add-coauthor" className={styles.paperDetails}>
+                  ADD SESSION
+                </label>
+              </>
+            )}
             {showForm && (
               <div className={styles.coAuthorForm}>
-                <h4 className={styles.paperDetails}>CO-AUTHOR </h4>
+                <h4 className={styles.paperDetails}>SESSION </h4>
                 <div className={styles.errorMessage}>
                   <p>{errorMessage}</p>
                 </div>
                 <div>
-                  <label htmlFor="email" className={styles.label}>
-                    EMAIL:
+                  <label htmlFor="name" className={styles.label}>
+                    Session Name:
+                  </label>
+                  <input
+                    id="name"
+                    className={styles.input}
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="location" className={styles.label}>
+                    Location:
                   </label>
                   <select
-                    id="email"
-                    name="email"
+                    id="location"
+                    name="location"
                     className={styles.affiliations}
-                    value={email}
-                    onChange={handleEmailChange}
+                    value={location}
+                    onChange={handleLocationChange}
                     defaultValue=""
                   >
                     <option disabled hidden value="">
-                      Choose Author Email
+                      Choose Session Location
                     </option>
-                    {emails?.map((em, key) => (
-                      <option key={key} onClick={handleEmailChange} value={em}>
+                    {locations?.map((em, key) => (
+                      <option
+                        key={key}
+                        onClick={handleLocationChange}
+                        value={em}
+                      >
                         {em}
                       </option>
                     ))}
                   </select>
                 </div>
                 <div>
-                  <label htmlFor="name" className={styles.label}>
-                    NAME:
+                  <label htmlFor="toTime" className={styles.label}>
+                    Starting Time:
                   </label>
                   <input
-                    id="name"
+                    id="toTime"
                     className={styles.input}
                     type="text"
-                    disabled
-                    value={name}
+                    onChange={(e) => setSTime(e.target.value)}
+                    value={sTime}
                   />
                 </div>
                 <div>
-                  <label htmlFor="affiliation" className={styles.label}>
-                    AFFILIATION:
-                  </label>
-                  <select
-                    id="affiliation"
-                    name="affiliation"
-                    className={styles.affiliations}
-                    value={affiliation}
-                    onChange={handleAffiliationChange}
-                    defaultValue=""
-                  >
-                    <option disabled hidden value="">
-                      Choose Affiliation
-                    </option>
-                    {affiliations?.map((affil, key) => (
-                      <option
-                        key={key}
-                        onClick={handleAffiliationChange}
-                        value={affil}
-                      >
-                        {affil}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className={styles.presenter}>
-                  <label htmlFor="mark-presenter" className={styles.label}>
-                    Mark as Presenter:
+                  <label htmlFor="fromTime" className={styles.label}>
+                    Finishing Time:
                   </label>
                   <input
-                    type="checkbox"
-                    id="mark-presenter"
-                    className={styles.checkbox}
-                    onChange={handleMarkPresenterChange}
-                    checked={markPresenter}
+                    id="fromTime"
+                    className={styles.input}
+                    type="text"
+                    onChange={(e) => setFTime(e.target.value)}
+                    value={fTime}
                   />
                 </div>
+
                 <Button
                   className={styles.saveBtn}
                   variant={1}
@@ -302,8 +347,96 @@ export default function UpdateSchedule() {
             )}
           </div>
         </div>
+
+        {/* <div>
+          <div>
+            {papers?.length > 0 && (
+              <>
+                <input
+                  type={"button"}
+                  onClick={handleShow2}
+                  value="+"
+                  id="add-coauthor"
+                  className={styles.addCoAuthorBtn}
+                ></input>
+                <label htmlFor="add-coauthor" className={styles.paperDetails}>
+                  ADD PAPER
+                </label>
+              </>
+            )}
+            {showForm2 && (
+              <div className={styles.coAuthorForm}>
+                <h4 className={styles.paperDetails}>PAPER </h4>
+                <div className={styles.errorMessage}>
+                  <p>{errorMessage2}</p>
+                </div>
+                <div>
+                  <label htmlFor="paper" className={styles.label}>
+                    Paper:
+                  </label>
+                  <select
+                    id="paper"
+                    name="paper"
+                    className={styles.affiliations}
+                    value={paper}
+                    onChange={handlePaperChange}
+                    defaultValue=""
+                  >
+                    <option disabled hidden value="">
+                      Choose Paper
+                    </option>
+                    {papers?.map((em, key) => (
+                      <option
+                        key={key}
+                        onClick={handlePaperChange}
+                        value={em.paperTitle}
+                      >
+                        {em.paperTitle}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="session" className={styles.label}>
+                    Session:
+                  </label>
+                  <select
+                    id="session"
+                    name="session"
+                    className={styles.affiliations}
+                    value={session2}
+                    onChange={handleSessionChange}
+                    defaultValue=""
+                  >
+                    <option disabled hidden value="">
+                      Choose Session
+                    </option>
+                    {sessions?.map((em, key) => (
+                      <option
+                        key={key}
+                        onClick={handleSessionChange}
+                        value={em.name}
+                      >
+                        {em.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <Button
+                  className={styles.saveBtn}
+                  variant={1}
+                  type="submit"
+                  text="Save"
+                  onClick={handleAdd2}
+                ></Button> 
+              </div>
+            )}
+          </div>
+        </div> */}
+
         <div className={styles.submitBtn}>
-          <Button variant={1} type="submit" text="Submit Paper"></Button>
+          <Button variant={1} type="submit" text="Update Schedule"></Button>
         </div>
       </form>
     </ContentContainer>
