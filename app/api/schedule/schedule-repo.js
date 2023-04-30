70646727404aa97de7c5a42fd765d4425418ddb0
hunzed <hunzalahhassan@gmail.com>
2023-04-30 09:28:32 +0300

    updated schedule api

--- app/api/schedule/schedule-repo.js
+++ app/api/schedule/schedule-repo.js
@@ -11,21 +11,26 @@ export default class ScheduleRepo {
         const schedule = JSON.parse(await fs.readFile(this.path));
         return schedule;
     }
-    async addScheduleItem (session) {
-        const allSessions = await this.getSchedule();
+    // async addScheduleItem (session) {
+    //     const allSessions = await this.getSchedule();
 
-        const matchingSession = allSessions.find(item => item.date === session.date);
-        if (matchingSession) {
-            matchingSession.presentations = session.presentations;
-        }
-        else {
-            allSessions.push(session);
-        }
+    //     const matchingSession = allSessions.find(item => item.date === session.date);
+    //     if (matchingSession) {
+    //         matchingSession.presentations = session.presentations;
+    //     }
+    //     else {
+    //         allSessions.push(session);
+    //     }
 
-        await fs.writeFile(this.path, JSON.stringify(allSessions));
-        return session;
+    //     await fs.writeFile(this.path, JSON.stringify(allSessions));
+    //     return session;
 
+    // }
+    async updateSchedule (schedule) {
+        await fs.writeFile(this.path, JSON.stringify(schedule));
+        return schedule;
     }
+    
     async getScheduleDates () {
         return JSON.parse(await fs.readFile(path.join(process.cwd(), "app/data/conference-dates.json")));
     }
