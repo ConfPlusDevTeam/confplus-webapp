68c44d6dd3d586d26b87035d362098be5dde1bee
Aly Soliman <alysoliman05@gmail.com>
2023-05-30 08:43:59 +0300

    feat: add bouncing motion to background

--- app/page.jsx
+++ app/page.jsx
@@ -30,99 +30,6 @@ export default function Home() {
     getSchedule();
   }, []);
 
-  // const schedule = [
-  //   {
-  //     name: "Day 1",
-  //     sessions: [
-  //       {
-  //         name: "Session 1",
-  //         fromTime: "9:00 AM",
-  //         presenters: [
-  //           {
-  //             name: "Presenter 1",
-  //           },
-  //           {
-  //             name: "Presenter 2",
-  //           },
-  //         ],
-  //         assignedPapers: [
-  //           {
-  //             title: "Paper 1",
-  //           },
-  //           {
-  //             title: "Paper 2",
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         name: "Session 2",
-  //         fromTime: "10:00 AM",
-  //         presenters: [
-  //           {
-  //             name: "Presenter 3",
-  //           },
-  //           {
-  //             name: "Presenter 4",
-  //           },
-  //         ],
-  //         assignedPapers: [
-  //           {
-  //             title: "Paper 3",
-  //           },
-  //           {
-  //             title: "Paper 4",
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: "Day 2",
-  //     sessions: [
-  //       {
-  //         name: "Session 1",
-  //         fromTime: "9:00 AM",
-  //         presenters: [
-  //           {
-  //             name: "Presenter 1",
-  //           },
-  //           {
-  //             name: "Presenter 2",
-  //           },
-  //         ],
-  //         assignedPapers: [
-  //           {
-  //             title: "Paper 1",
-  //           },
-  //           {
-  //             title: "Paper 2",
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         name: "Session 2",
-  //         fromTime: "10:00 AM",
-  //         presenters: [
-  //           {
-  //             name: "Presenter 3",
-  //           },
-  //           {
-  //             name: "Presenter 4",
-  //           },
-  //         ],
-  //         assignedPapers: [
-  //           {
-  //             title: "Paper 3",
-  //           },
-  //           {
-  //             title: "Paper 4",
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // ];
-
   return (
     <div className={styles.homeContainer}>
       <style>{`
@@ -140,11 +47,11 @@ export default function Home() {
       />
       <div className={styles.card}>
         {active == "welcome" && (
-          <ContentContainer variant={1} title="Welcome !" className={styles}>
-            We are delighted to have you attend the IEEE International Conference. This conference features some of the best minds in the field of Computer Science and Engineering. We hope you enjoy the conference and have a great time.
-            There will multiple sessions and paper presentations. You can find the schedule for the conference below.
-            
-          </ContentContainer>
+          <ContentContainer
+            variant={1}
+            title="Welcome !"
+            className={styles}
+          ></ContentContainer>
         )}
         {active == "schedule" && (
           <ContentContainer variant={2} title="Schedule" className={styles}>
@@ -153,7 +60,10 @@ export default function Home() {
         )}
         {active == "about" && (
           <ContentContainer variant={1} title="About" className={styles}>
-            Confplus is a webpage that simplifies the organization and management of the IEEE International Conference. Our user-friendly platform streamlines the planning process, from scheduling to promotion, to ensure a successful event.
+            Confplus is a webpage that simplifies the organization and
+            management of the IEEE International Conference. Our user-friendly
+            platform streamlines the planning process, from scheduling to
+            promotion, to ensure a successful event.
           </ContentContainer>
         )}
         <Scrollbar />
