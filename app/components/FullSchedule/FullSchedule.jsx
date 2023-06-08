c1a5a156612688310a8719cbc3164762fefa39c0
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-06-08 13:38:57 +0300

    fix

--- app/components/FullSchedule/FullSchedule.jsx
+++ app/components/FullSchedule/FullSchedule.jsx
@@ -52,9 +52,7 @@ export default function FullSchedule({ schedule }) {
                     >
                       <path d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
                     </svg>
-                    {new Date(session.fromTime).toLocaleTimeString() +
-                      " - " +
-                      new Date(session.endTime).toLocaleTimeString()}
+                    {session.fromTime + " - " + session.endTime}
                   </p>
                   <p>
                     <svg
@@ -64,13 +62,14 @@ export default function FullSchedule({ schedule }) {
                       <path d="M160 64c0-35.3 28.7-64 64-64H576c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H336.8c-11.8-25.5-29.9-47.5-52.4-64H384V320c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v32h64V64L224 64v49.1C205.2 102.2 183.3 96 160 96V64zm0 64a96 96 0 1 1 0 192 96 96 0 1 1 0-192zM133.3 352h53.3C260.3 352 320 411.7 320 485.3c0 14.7-11.9 26.7-26.7 26.7H26.7C11.9 512 0 500.1 0 485.3C0 411.7 59.7 352 133.3 352z" />
                     </svg>
                     Given by:{" "}
-                    {session.sessionPapers.map(
-                      (sessionPapers) =>
-                        sessionPapers.paper.presenter.first_name +
-                        " " +
-                        sessionPapers.paper.presenter.last_name
-                    )}
-                    {/* .join(", ")} */}
+                    {session.sessionPapers
+                      .map(
+                        (sessionPapers) =>
+                          sessionPapers.paper.presenter.first_name +
+                          " " +
+                          sessionPapers.paper.presenter.last_name
+                      )
+                      .join(", ")}
                   </p>
                   <p>
                     <svg
